5e14ca645f61f50a609e4e875d34c6039a1f8f25
michelleponceponce <62112590+michelleponceponce@users.noreply.github.com>
2021-04-01 22:07:36 -0600

    Capturar
    Creación de un sistema para capturar con javascript y bootstrap.
    Uso de funciones, clases...

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,115 +1,141 @@
-$(document).ready(function(){
-    calificacion()
+/*Banderas*/
+// sirven como señales, en donde la bandera capturar nos dira si podemos hacer algo o no, ´pueden ser variables o funciones las banderas
+// se puede definir una bandera con un return
+var capturar = false;
+
+/*Variables globales*/
+// la variable de tabledata que va a ser un string y se pone vacia
+var alumnos = [];
+var calificaciones = [];
+var posicion = 0;
+var tableData = "";
+var definicion;
+
+/*Elementos*/
+
+// variables que se ocupan para programar las funciones de los botones
+// BOTONES
+var buttonGuardar = document.getElementById("btnGuardar");
+var buttonLimpiar = document.getElementById("btnLimpiar");
+var buttonCapturar = document.getElementById("btnCapturar");
+
+// DIV
+var infoBox = document.getElementById("info");
+
+// INPUT
+var textNombre = document.getElementById("nombre");
+var textCalificacion = document.getElementById("calificacion");
+
+var tableBody = document.getElementById("data");
+
+/*Eventos globales*/
+buttonCapturar.addEventListener("click", function() {
+
+    capturar = !capturar;
+    capturaInformacion();
+
 });
 
-var opcion;
+buttonLimpiar.addEventListener("click", function() {
 
-function menu() {
-    console.log("Elige una opción");
-    console.log("1. Edad de moy");
-    console.log("2. Nombre de moy");
-    console.log("3. Lugar de nacimiento de Moy");
+    textNombre.value = '';
+    textCalificacion.value = '';
 
-    opcion =  Number(prompt("Elige una opción"));
+});
 
-    switch(opcion){
-        case 1:
-            console.log("26");
-            break;
-        case 2:
-            console.log("Moises ");
-            break
-        case 3:
-            console.log("Aguascalientes");
-        default:
-            console.log("Introduce una opcion");
-            break;
+buttonGuardar.addEventListener("click", function() {
+
+    if (textNombre.value.length > 0 && textCalificacion.value.length > 0) {
+        alumnos[posicion] = textNombre.value;
+        calificaciones[posicion] = textCalificacion.value;
+        if (textCalificacion.value == 10) {
+            tableData += `
+            <tr>
+                <td>${posicion+1}</td>
+                <td>${alumnos[posicion]}</td>
+                <td>${calificaciones[posicion]}</td>
+                <td class="btn btn-outline-primary">Sobresaliente</td>
+            </tr>
+        `;
+
+            tableBody.innerHTML = tableData;
+        }
+        if (textCalificacion.value == 9) {
+            tableData += `
+            <tr>
+                <td>${posicion+1}</td>
+                <td>${alumnos[posicion]}</td>
+                <td>${calificaciones[posicion]}</td>
+                <td class="btn btn-outline-success">Excelente</td>
+            </tr>
+        `;
+
+            tableBody.innerHTML = tableData;
+        }
+
+        if (textCalificacion.value >= 6 && textCalificacion.value <= 8) {
+            tableData += `
+            <tr>
+                <td>${posicion+1}</td>
+                <td>${alumnos[posicion]}</td>
+                <td>${calificaciones[posicion]}</td>
+                <td class="btn btn-outline-warning">Aceptable</td>
+            </tr>
+        `;
+
+            tableBody.innerHTML = tableData;
+        }
+
+        if (textCalificacion.value < 6) {
+            tableData += `
+            <tr>
+                <td>${posicion+1}</td>
+                <td>${alumnos[posicion]}</td>
+                <td>${calificaciones[posicion]}</td>
+                <td class="btn btn-outline-danger">Reprueba</td>
+            </tr>
+        `;
+
+            tableBody.innerHTML = tableData;
+        }
+
+        posicion++;
+
+    } else {
+        alert("Ingrese información válida")
     }
-}
+
+    textNombre.value = '';
+    textCalificacion.value = '';
+    console.log(alumnos);
+    console.log(calificaciones);
+});
 
 
-function saludar() {
-    var saludar= "si";
 
-    do {
-        console.log("hola");
-        saludar= prompt("saludar otra vez?");
+/*Funciones*/
+// si se da click en boton capturar se muestra los input, si se da de nuevo click se dejan de mostrar, empieza en el html con d-none
+// despues se cambia la clase de primary 
+// en la linea 87 donde dice buttoncapturar.innerHTML esta inyectando nuevo html a ese botón en cuando se le de click
+// doble negacion se convierte en verdadero
+function capturaInformacion() {
+
+    if (capturar) {
+
+        infoBox.classList.remove("d-none");
+        infoBox.classList.add("d-block");
+        buttonCapturar.classList.remove("btn-outline-primary");
+        buttonCapturar.classList.add("btn-outline-danger");
+        buttonCapturar.innerHTML = "Dejar de capturar";
+
+    } else if (!capturar) {
+
+        infoBox.classList.remove("d-block");
+        infoBox.classList.add("d-none");
+        buttonCapturar.classList.remove("btn-outline-danger");
+        buttonCapturar.classList.add("btn-outline-primary");
+        buttonCapturar.innerHTML = "Capturar";
 
     }
-    while (saludar== "si");
+
 }
-
-
-
-
-
-
-
-
-     
-    function calificacion(){
-    do {
-    otra = prompt("quieres poner otra?");
-    calif = Number(prompt("Digita tu calificación"));
-   
-    if (calif <= 5) {
-        console.log("estas reprobado");
-
-    } else if (calif > 5 ) {
-        console.log("Estas aprobado");
-
-    }
-
-   
-    }  while(otra == "si"); 
-
-    
-   
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  function eval(calif){
-  
-//    console.log("Elige");
-//    console.log("4. Estas reprobado");
-//    console.log("7. Pasaste");
-//    console.log("10. Excelente");
-  
-   
-//     do {
-        
-    
-       
-//     switch(calif) {
-//         case 4:
-//         console.log("Estas reprobado");
-//         break;
-//         case 7:
-//         console.log("Pasaste");
-//         break;
-//         case 10:
-//         console.log("Excelente");
-//         break;
-//         }
-//         otra = prompt("quieres poner otra?");
-//         calif = Number(prompt("Digite su calificación"));
-//     }while  (otra == "si");
-
-// } 
-   
-
- 
- 
-
